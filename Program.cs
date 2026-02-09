397d4127b1b5d0825319dd6f49ae6f54d6e62a94
LucassSantoss <lucascross.pereirasantos@gmail.com>
2026-02-09 19:39:09 -0300

    feat: first commit

---
+++ Program.cs
@@ -0,0 +1,25 @@
+using Microsoft.OpenApi;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen(options =>
+{
+    options.SwaggerDoc("v1", new OpenApiInfo { Title = "Auth API", Version = "v1" });
+});
+builder.Services.AddControllers();
+
+var app = builder.Build();
+
+app.MapGet("/", () => "Hello World!");
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI(options => options.SwaggerEndpoint("/swagger/v1/swagger.json", "Auth API V1"));
+}
+
+app.UseHttpsRedirection();
+app.MapControllers();
+
+app.Run();
